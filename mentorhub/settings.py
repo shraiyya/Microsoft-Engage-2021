d8eba4b23577d387c0e9f26719c10a717e13d6e0
Shreya Pawaskar <shreya.pawaskar@cumminscollege.in>
2021-11-28 09:20:41 +0530

    Update settings.py

--- mentorhub/settings.py
+++ mentorhub/settings.py
@@ -101,7 +101,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=[STATIC_DIR,]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+MEDIA_ROOT=os.path.join(BASE_DIR,'static_media')
 
 
 
